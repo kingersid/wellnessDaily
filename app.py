c41e02d8244ac239df248e9fa23eef6a2858d3ef
kingersid <kinger.siddharth@gmail.com>
2023-05-21 10:02:48 +0530

    going remote

--- app.py
+++ app.py
@@ -1,65 +1,63 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
+from flaskext.markdown import Markdown
+
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+Markdown(app)   
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog1.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+
+
+
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    title = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.String(200), nullable=False)
+    author= db.Column(db.String(200), nullable=False)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<post %r>' % self.body
 
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/add_new_post', methods=['POST', 'GET'])
+def new_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body=request.form['body']
+        author=request.form['author']
+
+        new_post = Post(title=title,body=body,author=author)
+
+        try:
+            db.session.add(new_post)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your post'
+    
+    return render_template('update.html')
+
+
+
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        task_content = request.form['content']
-        new_task = Todo(content=task_content)
+    posts = Post.query.order_by(Post.title).all()
+    return render_template('index.html', posts=posts)
 
-        try:
-            db.session.add(new_task)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue adding your task'
+@app.route('/aboutme')
+def about_me():
+    bio='''Siddharth Kinger is a passionate fitness enthusiast and successful businessman. He has dedicated his life to maintaining a healthy lifestyle while excelling in his business ventures. With a deep love for fitness, Siddharth believes in the power of physical activity to enhance both personal and professional life.
 
-    else:
-        tasks = Todo.query.order_by(Todo.date_created).all()
-        return render_template('index.html', tasks=tasks)
+As a fitness enthusiast, Siddharth actively engages in various workout routines, including weightlifting, cardio exercises, and yoga. He emphasizes the importance of regular exercise, proper nutrition, and a balanced lifestyle to achieve optimal physical and mental well-being.
 
+In addition to his commitment to fitness, Siddharth is also a seasoned businessman. With years of experience in the industry, he has achieved significant success in his entrepreneurial endeavors. His strong work ethic, strategic mindset, and ability to overcome challenges have contributed to his accomplishments in the business world.
 
-@app.route('/delete/<int:id>')
-def delete(id):
-    task_to_delete = Todo.query.get_or_404(id)
+Siddharth's passion for fitness and business motivates him to inspire and empower others. Through his expertise and practical knowledge, he shares valuable insights on achieving work-life balance, setting and achieving fitness goals, and unlocking personal and professional potential.
 
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-
-@app.route('/update/<int:id>', methods=['GET', 'POST'])
-def update(id):
-    task = Todo.query.get_or_404(id)
-
-    if request.method == 'POST':
-        task.content = request.form['content']
-
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-
-    else:
-        return render_template('update.html', task=task)
-
+Combining his love for fitness and his business acumen, Siddharth aims to make a positive impact by encouraging individuals to prioritize their health while pursuing their entrepreneurial dreams. His dedication, discipline, and determination serve as an inspiration to those striving for success in both their personal and professional lives.'''
+    return render_template('aboutme.html',bio=bio)
 
 if __name__ == "__main__":
     app.run(debug=True)
