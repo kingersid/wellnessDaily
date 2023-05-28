037c3374d41d89990b1cad41249ac4fd8bbd8f46
kingersid <kinger.siddharth@gmail.com>
2023-05-28 14:52:09 +0530

    updated typo

--- data.py
+++ data.py
@@ -5,7 +5,7 @@ conn=sqlite3.connect('blog1.db')
 cursor = conn.cursor()
 
 
-update_query = " UPDATE postF SET image = 'skip.png'  WHERE id = 3"
+update_query = " UPDATE postF SET title = 'Skipping: An Effective Exercise for Fat Loss and its Benefits'  WHERE id = 3"
 
 cursor.execute(update_query)
 
