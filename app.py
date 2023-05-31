7757a0a2ef655b39d281e67b3221019813d7fe62
kingersid <kinger.siddharth@gmail.com>
2023-05-31 10:57:26 +0530

    added tags

--- app.py
+++ app.py
@@ -7,22 +7,35 @@ app = Flask(__name__)
  
 Markdown(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog1.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog2.db'
 db = SQLAlchemy(app)
 
 
 
 
-class PostF(db.Model):
+class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
     body = db.Column(db.String(200), nullable=False)
     author= db.Column(db.String(200), nullable=False)
     date= db.Column(db.String(200), nullable=False)
     image=db.Column(db.String(200), nullable=False)
+    tags = db.relationship('Tag', secondary='post_tags', backref=db.backref('posts', lazy='dynamic'))
 
     def __repr__(self):
         return '<post %r>' % self.body
+
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True)
+
+    def __repr__(self):
+        return '<Tag {}>'.format(self.name)
+    
+post_tags = db.Table('post_tags',
+    db.Column('post_id', db.Integer, db.ForeignKey('post.id'), primary_key=True),
+    db.Column('tag_id', db.Integer, db.ForeignKey('tag.id'), primary_key=True)
+)
 
 
 @app.route('/add_new_post', methods=['POST', 'GET'])
@@ -33,38 +46,53 @@ def new_post():
         author=request.form['author']
         date=request.form['date']
         image=request.form['image']
+        tags=request.form['tags']
+        tags_list=tags.split(',')
 
-        new_post = PostF(title=title,body=body,author=author,date=date,image=image)
+        
+        post = Post(title=title,body=body,author=author,date=date,image=image)
+        post.tags=[Tag(name=tag_name) for tag_name in tags_list ]
 
         try:
-            db.session.add(new_post)
-            db.session.commit()
-            return redirect('/')
+            db.session.add(post)
         except:
-            return 'There was an issue adding your post'
+            return 'there was an error'        
+        db.session.commit()
+        return redirect('/')
+        
     
     return render_template('update.html')
 
 
 @app.route('/')
 def index():
-    css_file = url_for('static', filename='css/styles.css', v='1.15233')
-    posts = PostF.query.order_by(PostF.title).all()
+    css_file = url_for('static', filename='css/styles.css', v='1.15238')
+    posts = Post.query.order_by(Post.title).all()
     return render_template('index.html', posts=posts,css_file=css_file)
 
 
 @app.route('/post/<int:id>')
 def show_post(id):
     # Fetch the blog post information from the database or wherever you store it
-    post = PostF.query.filter_by(id=id)
-    css_file = url_for('static', filename='css/styles.css', v='1.15234')
+    post = Post.query.filter_by(id=id)
+    css_file = url_for('static', filename='css/styles.css', v='1.15238')
     # Render the template with the post dat
     return render_template('post.html',post=post,css_file=css_file)
 
+@app.route('/tag/<tag_name>')
+def showPostByTag(tag_name):
+    if tag_name:
+        posts = Post.query.join(Post.tags).filter(Tag.name == tag_name).all()
+    else:
+        posts = Post.query.all()
+
+    css_file = url_for('static', filename='css/styles.css', v='1.15238')    
+    return render_template('index.html',posts=posts,css_file=css_file)
+                
 
 @app.route('/aboutme')
 def about_me():
-    css_file = url_for('static', filename='css/styles.css', v='1.1523357')
+    css_file = url_for('static', filename='css/styles.css', v='1.1523359')
     bio='''Siddharth Kinger is a passionate fitness enthusiast and successful businessman. He has dedicated his life to maintaining a healthy lifestyle while excelling in his business ventures. With a deep love for fitness, Siddharth believes in the power of physical activity to enhance both personal and professional life.
 
 As a fitness enthusiast, Siddharth actively engages in various workout routines, including weightlifting, cardio exercises, and yoga. He emphasizes the importance of regular exercise, proper nutrition, and a balanced lifestyle to achieve optimal physical and mental well-being.
--- data.py
+++ data.py
@@ -1,16 +1,18 @@
 import sqlite3
 
-conn=sqlite3.connect('blog1.db')
+# Connect to the SQLite database
+conn = sqlite3.connect('blog2.db')
 
+# Create a cursor object to execute SQL commands
 cursor = conn.cursor()
 
+# Create the table to store tags
+# Execute the DELETE query to remove all rows from the table
+table_name = 'post'
+delete_query = f"DELETE FROM {table_name}"
+cursor.execute(delete_query)
 
-update_query = " UPDATE postF SET title = 'Skipping: An Effective Exercise for Fat Loss and its Benefits'  WHERE id = 3"
-
-cursor.execute(update_query)
-
+# Commit the changes and close the connection
 conn.commit()
-
-cursor.close()
 conn.close()
 
